720317f876f01fc49b0b00a25c98a8b979407856
wilckmaia <153649327+wilckmaia@users.noreply.github.com>
2025-01-17 22:27:19 +0000

    finish `/api/v1/status` endpoint

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_At).toISOString();
+  expect(responseBody.update_At).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("17.2");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.openned_connections).toEqual(1);
 });
